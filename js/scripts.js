005b4fe40dfc377b3fee15eecf2ca7c35d9b7d4d
Stephanie <cherrygurl5000@yahoo.com>
2024-05-26 12:40:49 -0400

    Completed the homepage functionality and expanding boxes. Still need to add page links.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-// document.write('YO');
-
 // Show the menu when the menu button is clicked
 $('#menuIcon').click(() => {
     $('.topNav').fadeToggle(800, ()=> {
@@ -13,16 +11,71 @@ $('#menuIcon').click(() => {
         
 });
 
-// Make both images the same size
-setTimeout(() => $('#dcImg').height($('#marvelImg').height()), 100);
+// Make both universe images the same size
+setTimeout(() => $('#dcImg').height($('#marvelImg').height()), 300);
 
-// Expand the universes when the universe is hovered over
-/*let left = $('#noLeftBorder');
-let right = $('#noRightBorder');
+// function for label placement
+function labelPlacement() {
+    // Place the labels based on the image size
+    $('.allLH').css('top', $('#allHeroesImg').height() - ($('.allLH').height()*0.4));
+    $('.allLH').css('left', 0);
+    $('.dcLH').css('top', $('#dcImg').height() + 8);
+    $('.dcLH').css('left', 0);
+    $('.marvelLH').css('top', $('#marvelImg').height() + 8);
+    $('.marvelLH').css('left', 0);
+}
 
+// Place the labels after the page is fully loaded and the images are properly placed
 $(document).ready(function () {
-$('#noLeftBorder').on('mouseenter', function() { console.log('left entered')});
-$('#noRightBorder').mouseover(function() { console.log('right entered')});
-});*/
+    setTimeout(labelPlacement, 500);
+});
 
+// function for expanding the correctly hovered box and shrinking the other boxes
+function hovering() {
+    let hovered = "#" + $(this).attr('id');
+    let allElem = ['#noLeftBorder', '#noRightBorder', '#allSect'];
+    let notHovered = allElem.filter(i => i != hovered);
+
+    $('.labelHeading').css('visibility', 'hidden');
+
+    $(notHovered[0]).switchClass('col-md-4', 'col-md-3');
+    $(notHovered[1]).switchClass('col-md-4', 'col-md-3');
+    $(hovered).switchClass('col-md-4', 'col-md-6');
+
+};
+
+// function to reset the univerese classes to equal
+function noHover() {
+    let hovered = "#" + $(this).attr('id');
+    let allElem = ['#noLeftBorder', '#noRightBorder', '#allSect'];
+    let notHovered = allElem.filter(i => i != hovered);
+
+    $(hovered).removeClass('col-md-6');
+    $(hovered).addClass('col-md-4');
+
+    $(notHovered[0]).removeClass('col-md-3');
+    $(notHovered[0]).addClass('col-md-4');
+
+    $(notHovered[1]).removeClass('col-md-3');
+    $(notHovered[1]).addClass('col-md-4');
+
+    $('.labelHeading').css('visibility', 'visible');
+}
+
+// Only do the following if on a medium or larger screen
+if ($(window).width() >= 768) {
+    $('.allSection').removeClass('row');
+    $('.allSection').addClass('col-md-4');
+    $('.allSection a figure').removeClass('w-50');
+    $('.allSection a figure').addClass('w-75');
+    
+    // Expand the universes when the universe is hovered over
+    $('#noLeftBorder').mouseover(hovering);
+    $('#noRightBorder').mouseover(hovering);
+    $('#allSect').mouseover(hovering);
+
+    $('#noLeftBorder').mouseleave(noHover);
+    $('#noRightBorder').mouseleave(noHover);
+    $('#allSect').mouseleave(noHover);
+}
 
