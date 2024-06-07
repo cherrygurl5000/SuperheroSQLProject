b5d90b91a4811d9801a73675e1e388459a7d2172
Stephanie <cherrygurl5000@yahoo.com>
2024-06-07 19:55:28 -0400

    All functionality and styling completed.

--- html/heroPage.php
+++ html/heroPage.php
@@ -10,11 +10,13 @@
 
 <?php require './include/header.php'; ?>
 <?php 
+    // Connect to the database and collect all information about the chosen character
     $con = new mysqli(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
     if ($con->connect_error){
         die("Connection failed!");
     }
     else {
+        // Set the id to 1 if no id or an id outside of the range is given 
         $id = 1;
         if(isset($_GET['id'])) {
             $sqlSelectAll = "SELECT * FROM hero";  
@@ -48,6 +50,7 @@
         }
     }        
     $con->close();
+    // Add the team name to the team image
     if($teamName == 'Avengers') {
         $teamImg = 'avengersTeam-img.webp';
     }
@@ -57,6 +60,15 @@
     else {
         $teamImg = 'otherTeam-img.jpg';
     }
+    // Turn the powers, abilities, weaknesses, and enemies into arrays
+    $heroPwr = explode(',', $heroPwr);
+    $heroAb = explode(',', $heroAb);
+    $heroWeak = explode(',', $heroWeak);
+    $heroEn = explode(',', $heroEn);
+    // Replace the special character stripped from the bio with '
+    //echo(str_replace("\&#039;", "333", $heroBio));
+    $heroBio = html_entity_decode($heroBio);
+    $heroBio = str_replace('\\', '', $heroBio);
 ?>
 <body class="container-md heroPg" style="background-image: url('../logos/<?php echo($logoLoc); ?>')">
 
@@ -69,7 +81,7 @@
                 <img src="../imgs/<?php echo($imgLoc); ?>" alt="<?php echo($heroName); ?> Image" class="heroImg" id="heroId">
             </figure>
         </section>
-        <section class="col-7 col-lg-9 h-md-100">
+        <section class="col-7 col-lg-9 h-md-100 align-self-center">
             <h2 class="infoTitles" id="bioTitle">Bio</h2>
             <p class="overflow-auto infoText" id="bioText">
             <?php echo($heroBio); ?>
@@ -78,41 +90,49 @@
         <section class="col-6 col-md-3 mt-3">
             <h2 class="infoTitles">Powers</h2>
             <ul class="overflow-auto infoText skillsText">
-                <li>Powers</li>
-                <li>Powers</li>
-                <li>Powers</li>
-                <li>Powers</li>
-                <li>Powers</li>
+                <?php 
+                    foreach($heroPwr as $power) {
+                        ?>
+                <li><?php echo($power); ?></li>
+                        <?php
+                    } 
+                ?>
             </ul>
         </section>
         <section class="col-6 col-md-3 mt-3">
             <h2 class="infoTitles">Abilities</h2>
             <ul class="overflow-auto infoText skillsText">
-                <li>Abilities</li>
-                <li>Abilities</li>
-                <li>Abilities</li>
-                <li>Abilities</li>
-                <li>Abilities</li>
+                <?php 
+                    foreach($heroAb as $ability) {
+                        ?>
+                <li><?php echo($ability); ?></li>
+                        <?php
+                    } 
+                ?>
             </ul>
         </section>
         <section class="col-6 col-md-3 mt-md-3">
             <h2 class="infoTitles">Weaknesses</h2>
             <ul class="overflow-auto infoText skillsText">
-                <li>Weaknesses</li>
-                <li>Weaknesses</li>
-                <li>Weaknesses</li>
-                <li>Weaknesses</li>
-                <li>Weaknesses</li>
+                <?php 
+                    foreach($heroWeak as $weak) {
+                        ?>
+                <li><?php echo($weak); ?></li>
+                        <?php
+                    } 
+                ?>
             </ul>
         </section>
         <section class="col-6 col-md-3 mt-md-3">
             <h2 class="infoTitles">Enemies</h2>
             <ul class="overflow-auto infoText skillsText">
-                <li>Enemies</li>
-                <li>Enemies</li>
-                <li>Enemies</li>
-                <li>Enemies</li>
-                <li>Enemies</li>
+                <?php 
+                    foreach($heroEn as $enemy) {
+                        ?>
+                <li><?php echo($enemy); ?></li>
+                        <?php
+                    } 
+                ?>
             </ul>
         </section>
         <section class="col-10 col-md-4">
